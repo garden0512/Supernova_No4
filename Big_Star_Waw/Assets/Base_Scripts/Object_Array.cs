b3df923cd265668b8e3ecd56ed17539e09316d35
기슨 <rlghks7156@naver.com>
2024-09-30 19:48:06 +0900

    array
    플레이어 레이어 정렬 추가

---
+++ Big_Star_Waw/Assets/Base_Scripts/Object_Array.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Array : MonoBehaviour
+{
+    private float xpos;
+    private float ypos;
+    private float objectHeight;
+
+    private Transform tf;
+    private Renderer rend;
+
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        rend = GetComponent<Renderer>();
+
+        if (rend != null)
+        {
+            objectHeight = rend.bounds.size.y;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xpos = tf.position.x;
+        ypos = tf.position.y;
+
+        float bottomY = ypos - objectHeight / 2 + 0.3f;
+        tf.position = new Vector3(xpos, ypos, bottomY / 1000.0f);
+    }
+}
--- Big_Star_Waw/Assets/Monster/Natti/Natti_AI.cs
+++ Big_Star_Waw/Assets/Monster/Natti/Natti_AI.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Natti_AI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float stopDistance;
+
+    private Transform target;
+
+    private void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Vector2.Distance(transform.position, target.position) > stopDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
     }
 }
